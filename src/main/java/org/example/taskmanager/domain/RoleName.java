d5bb83e551a3c5348f736d8e30031ff086b31deb
Azazagich <qwertyu2289@ukr.net>
2024-09-09 14:27:24 +0300

    add: final class domain.RoleName class which contain all roles which we can initialize users.

---
+++ src/main/java/org/example/taskmanager/domain/RoleName.java
@@ -0,0 +1,12 @@
+package org.example.taskmanager.domain;
+
+public final class RoleName {
+
+    public static final String USER = "USER";
+
+    public static final String MANAGER = "MANAGER";
+
+    public static final String ADMIN = "ADMIN";
+
+    public static final String ANONYMOUS = "ANONYMOUS";
+}
