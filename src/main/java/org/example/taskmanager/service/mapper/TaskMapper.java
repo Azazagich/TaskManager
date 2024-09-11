68d831994633ebbf5ae073dc3c9912367174c203
Azazagich <qwertyu2289@ukr.net>
2024-09-11 16:10:25 +0300

    add: method toTaskDTO which return Optional in class TaskMapper

--- src/main/java/org/example/taskmanager/service/mapper/TaskMapper.java
+++ src/main/java/org/example/taskmanager/service/mapper/TaskMapper.java
@@ -1,26 +1,23 @@
 package org.example.taskmanager.service.mapper;
 
-import org.example.taskmanager.domain.Status;
-import org.example.taskmanager.domain.Tag;
 import org.example.taskmanager.domain.Task;
-import org.example.taskmanager.service.dto.StatusDTO;
-import org.example.taskmanager.service.dto.TagDTO;
 import org.example.taskmanager.service.dto.TaskDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface TaskMapper {
 
-    TaskMapper INSTANCE = Mappers.getMapper(TaskMapper.class);
-
     Task toTask(TaskDTO taskDTO);
+
+    List<Task> toTasks(List<TaskDTO> taskDTOS);
 
     TaskDTO toTaskDTO(Task task);
 
-    List<TaskDTO> toTaskDTOS(List<Task> task);
+    Optional<TaskDTO> toTaskDTO(Optional<Task> tag);
 
-    List<Task> toTasks(List<TaskDTO> taskDTOS);
+    List<TaskDTO> toTaskDTOS(List<Task> task);
 }
