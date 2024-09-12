b41dd69a57a77988731c75c80cbf949d40429050
Azazagich <qwertyu2289@ukr.net>
2024-09-12 12:20:45 +0300

    add: methods partialUpdate and fullUpdate for interface UserMapper

--- src/main/java/org/example/taskmanager/service/mapper/UserMapper.java
+++ src/main/java/org/example/taskmanager/service/mapper/UserMapper.java
@@ -2,9 +2,17 @@ package org.example.taskmanager.service.mapper;
 
 import org.example.taskmanager.domain.User;
 import org.example.taskmanager.service.dto.UserDTO;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper
 public interface UserMapper extends MapperEntity<User, UserDTO>{
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void partialUpdate(UserDTO dto, @MappingTarget User entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    void fullUpdate(UserDTO dto, @MappingTarget User entity);
 }
