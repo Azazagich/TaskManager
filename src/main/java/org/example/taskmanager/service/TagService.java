b427075df1945f9308b5c5f3aa7c59eec48af8c7
Azazagich <qwertyu2289@ukr.net>
2024-09-12 12:22:29 +0300

    add: TagService class and implemented CrudService methods

---
+++ src/main/java/org/example/taskmanager/service/TagService.java
@@ -0,0 +1,62 @@
+package org.example.taskmanager.service;
+
+import org.example.taskmanager.domain.Tag;
+import org.example.taskmanager.repository.TagRepository;
+import org.example.taskmanager.service.dto.TagDTO;
+import org.example.taskmanager.service.mapper.TagMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+
+@Service
+@Transactional
+public class TagService implements CrudService<TagDTO, Long>{
+
+    private final TagRepository tagRepository;
+
+    private final TagMapper tagMapper;
+
+    @Autowired
+    public TagService(TagRepository tagRepository, TagMapper tagMapper){
+        this.tagRepository = tagRepository;
+        this.tagMapper = tagMapper;
+    }
+
+    @Override
+    public TagDTO getById(Long id) {
+        return tagMapper.toDTO(tagRepository.findById(id)).orElseThrow();
+    }
+
+    @Override
+    public List<TagDTO> getAll() {
+        return tagMapper.toDTOS(tagRepository.findAll());
+    }
+
+    @Override
+    public TagDTO save(TagDTO tagDTO) {
+        return tagMapper.toDTO(tagRepository.save(tagMapper.toEntity(tagDTO)));
+    }
+
+    @Override
+    public boolean updateAll(TagDTO tagDTO) {
+        Tag tag = tagRepository.findById(tagDTO.getId()).orElseThrow();
+        tagMapper.fullUpdate(tagDTO, tag);
+        tagRepository.save(tag);
+        return true;
+    }
+
+    @Override
+    public boolean update(TagDTO tagDTO) {
+        Tag tag = tagRepository.findById(tagDTO.getId()).orElseThrow();
+        tagMapper.partialUpdate(tagDTO, tag);
+        tagRepository.save(tag);
+        return true;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        tagRepository.deleteById(id);
+    }
+}
