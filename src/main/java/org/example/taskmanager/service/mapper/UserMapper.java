79aa1c04d8f43755814ee64eb3e27ab8f0f212da
Azazagich <qwertyu2289@ukr.net>
2024-09-12 10:53:45 +0300

    update: UserMapper extends from interface MapperEntity

--- src/main/java/org/example/taskmanager/service/mapper/UserMapper.java
+++ src/main/java/org/example/taskmanager/service/mapper/UserMapper.java
@@ -1,23 +1,10 @@
 package org.example.taskmanager.service.mapper;
 
-import org.example.taskmanager.domain.Task;
 import org.example.taskmanager.domain.User;
-import org.example.taskmanager.service.dto.TaskDTO;
 import org.example.taskmanager.service.dto.UserDTO;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapper;
 
-import java.util.List;
-import java.util.Optional;
+@Mapper
+public interface UserMapper extends MapperEntity<User, UserDTO>{
 
-public interface UserMapper {
-
-    User toUser(UserDTO userDTO);
-
-    List<UserDTO> toUserDTOS(List<User> user);
-
-    UserDTO toUserDTO(User user);
-
-    Optional<UserDTO> toUserDTO(Optional<User> user);
-
-    List<User> toUsers(List<UserDTO> userDTOS);
 }
