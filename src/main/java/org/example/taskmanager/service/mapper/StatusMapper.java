89ee758f6fe0a783016c9df7005aab0ce36c931d
Azazagich <qwertyu2289@ukr.net>
2024-09-12 10:52:57 +0300

    update: StatusMapper extends from interface MapperEntity

--- src/main/java/org/example/taskmanager/service/mapper/StatusMapper.java
+++ src/main/java/org/example/taskmanager/service/mapper/StatusMapper.java
@@ -3,21 +3,8 @@ package org.example.taskmanager.service.mapper;
 import org.example.taskmanager.domain.Status;
 import org.example.taskmanager.service.dto.StatusDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
-
-import java.util.List;
-import java.util.Optional;
 
 @Mapper
-public interface StatusMapper {
+public interface StatusMapper extends MapperEntity<Status, StatusDTO>{
 
-    Status toStatus(StatusDTO statusDTO);
-
-    List<Status> toStatus(List<StatusDTO> statusesDTO);
-
-    StatusDTO toStatusDTO(Status status);
-
-    Optional<StatusDTO> toStatusDTO(Optional<Status> status);
-
-    List<StatusDTO> toStatusDTOS(List<Status> status);
 }
