efa87fd239466b072523c5fd4787ca4b5b7140ab
Azazagich <qwertyu2289@ukr.net>
2024-09-12 12:18:33 +0300

    add: methods partialUpdate and fullUpdate for interface StatusMapper

--- src/main/java/org/example/taskmanager/service/mapper/StatusMapper.java
+++ src/main/java/org/example/taskmanager/service/mapper/StatusMapper.java
@@ -2,9 +2,17 @@ package org.example.taskmanager.service.mapper;
 
 import org.example.taskmanager.domain.Status;
 import org.example.taskmanager.service.dto.StatusDTO;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper
 public interface StatusMapper extends MapperEntity<Status, StatusDTO>{
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void partialUpdate(StatusDTO dto, @MappingTarget Status entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    void fullUpdate(StatusDTO dto, @MappingTarget Status entity);
 }
