1518045a1b188a63b09987481119c40866942786
Azazagich <qwertyu2289@ukr.net>
2024-09-12 12:23:03 +0300

    add: UserService class and implemented CrudService methods

---
+++ src/main/java/org/example/taskmanager/service/UserService.java
@@ -0,0 +1,65 @@
+package org.example.taskmanager.service;
+
+import org.example.taskmanager.domain.Role;
+import org.example.taskmanager.domain.User;
+import org.example.taskmanager.repository.TaskRepository;
+import org.example.taskmanager.repository.UserRepository;
+import org.example.taskmanager.service.dto.UserDTO;
+import org.example.taskmanager.service.mapper.TaskMapper;
+import org.example.taskmanager.service.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService implements CrudService<UserDTO, Long>{
+
+    private final UserRepository userRepository;
+
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserService(UserRepository userRepository, UserMapper userMapper){
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public UserDTO getById(Long id) {
+        return userMapper.toDTO(userRepository.findById(id)).orElseThrow();
+    }
+
+    @Override
+    public List<UserDTO> getAll() {
+        return userMapper.toDTOS(userRepository.findAll());
+    }
+
+    @Override
+    public UserDTO save(UserDTO userDTO) {
+        return userMapper.toDTO(userRepository.save(userMapper.toEntity(userDTO)));
+    }
+
+    @Override
+    public boolean updateAll(UserDTO userDTO) {
+        User user = userRepository.findById(userDTO.getId()).orElseThrow();
+        userMapper.fullUpdate(userDTO, user);
+        userRepository.save(user);
+        return true;
+    }
+
+    @Override
+    public boolean update(UserDTO userDTO) {
+        User user = userRepository.findById(userDTO.getId()).orElseThrow();
+        userMapper.partialUpdate(userDTO, user);
+        userRepository.save(user);
+        return true;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
