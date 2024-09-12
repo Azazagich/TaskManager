aff9ea543c51a6d7a9e2ea41afc8be1591485c95
Azazagich <qwertyu2289@ukr.net>
2024-09-12 10:53:05 +0300

    update: TagMapper extends from interface MapperEntity

--- src/main/java/org/example/taskmanager/service/mapper/TagMapper.java
+++ src/main/java/org/example/taskmanager/service/mapper/TagMapper.java
@@ -1,25 +1,10 @@
 package org.example.taskmanager.service.mapper;
 
-import org.example.taskmanager.domain.Status;
 import org.example.taskmanager.domain.Tag;
-import org.example.taskmanager.service.dto.StatusDTO;
 import org.example.taskmanager.service.dto.TagDTO;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
-
-import java.util.List;
-import java.util.Optional;
 
 @Mapper
-public interface TagMapper {
+public interface TagMapper extends MapperEntity<Tag, TagDTO>{
 
-    Tag toTag(TagDTO tagDTO);
-
-    List<Tag> toTags(List<TagDTO> tagDTO);
-
-    TagDTO toTagDTO(Tag tag);
-
-    Optional<TagDTO> toTagDTO(Optional<Tag> tag);
-
-    List<TagDTO> toTagsDTOS(List<Tag> tags);
 }
