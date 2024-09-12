299bdda0536534712158760b7de9bb2512bb7787
Azazagich <qwertyu2289@ukr.net>
2024-09-12 10:53:34 +0300

    update: TaskMapper extends from interface MapperEntity

--- src/main/java/org/example/taskmanager/service/mapper/TaskMapper.java
+++ src/main/java/org/example/taskmanager/service/mapper/TaskMapper.java
@@ -4,20 +4,7 @@ import org.example.taskmanager.domain.Task;
 import org.example.taskmanager.service.dto.TaskDTO;
 import org.mapstruct.Mapper;
 
-
-import java.util.List;
-import java.util.Optional;
-
 @Mapper
-public interface TaskMapper {
+public interface TaskMapper extends MapperEntity<Task, TaskDTO>{
 
-    Task toTask(TaskDTO taskDTO);
-
-    List<Task> toTasks(List<TaskDTO> taskDTOS);
-
-    TaskDTO toTaskDTO(Task task);
-
-    Optional<TaskDTO> toTaskDTO(Optional<Task> tag);
-
-    List<TaskDTO> toTaskDTOS(List<Task> task);
 }
