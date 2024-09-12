2ae7ef605f89372a17727e2881e88a4917205d61
Azazagich <qwertyu2289@ukr.net>
2024-09-12 12:18:49 +0300

    add: methods partialUpdate and fullUpdate for interface TagMapper

--- src/main/java/org/example/taskmanager/service/mapper/TagMapper.java
+++ src/main/java/org/example/taskmanager/service/mapper/TagMapper.java
@@ -2,9 +2,17 @@ package org.example.taskmanager.service.mapper;
 
 import org.example.taskmanager.domain.Tag;
 import org.example.taskmanager.service.dto.TagDTO;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper
 public interface TagMapper extends MapperEntity<Tag, TagDTO>{
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void partialUpdate(TagDTO dto, @MappingTarget Tag entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+    void fullUpdate(TagDTO dto, @MappingTarget Tag entity);
 }
