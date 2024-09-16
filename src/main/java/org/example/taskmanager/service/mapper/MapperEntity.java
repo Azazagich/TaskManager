695d9192c19f4ede82771e1510cb3569586d01da
Azazagich <qwertyu2289@ukr.net>
2024-09-16 17:13:06 +0300

    remove: method Optional toDTO from MapperEntity.class

--- src/main/java/org/example/taskmanager/service/mapper/MapperEntity.java
+++ src/main/java/org/example/taskmanager/service/mapper/MapperEntity.java
@@ -1,10 +1,7 @@
 package org.example.taskmanager.service.mapper;
 
-import org.mapstruct.Mapper;
 import java.util.List;
-import java.util.Optional;
 
-@Mapper
 public interface MapperEntity <Entity, DTO>{
 
     Entity toEntity(DTO dto);
@@ -12,8 +9,6 @@ public interface MapperEntity <Entity, DTO>{
     List<Entity> toEntities(List<DTO> dtos);
 
     DTO toDTO(Entity entity);
-
-    Optional<DTO> toDTO(Optional<Entity> entity);
 
     List<DTO> toDTOS(List<Entity> entities);
 }
