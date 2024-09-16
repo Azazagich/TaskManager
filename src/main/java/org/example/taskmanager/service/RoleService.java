b1889dece8c7d268e445e5d9d847907b7d9ea772
Azazagich <qwertyu2289@ukr.net>
2024-09-16 17:14:53 +0300

    update: our methods in RoleService

--- src/main/java/org/example/taskmanager/service/RoleService.java
+++ src/main/java/org/example/taskmanager/service/RoleService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 @Transactional
@@ -18,14 +19,14 @@ public class RoleService implements CrudService<RoleDTO, Long>{
     private final RoleMapper roleMapper;
 
     @Autowired
-    public RoleService(RoleRepository roleRepository, RoleMapper roleMapper){
+    public RoleService(RoleRepository roleRepository,RoleMapper roleMapper){
         this.roleRepository = roleRepository;
         this.roleMapper = roleMapper;
     }
 
     @Override
     public RoleDTO getById(Long id) {
-        return roleMapper.toDTO(roleRepository.findById(id)).orElseThrow();
+        return roleMapper.toDTO(roleRepository.findById(id).orElseThrow());
     }
 
     @Override
@@ -41,8 +42,8 @@ public class RoleService implements CrudService<RoleDTO, Long>{
     //full update це без перевірки на null просто сетається Full Update (Повне оновлення)
     //Повне оновлення означає, що всі поля DTO повинні перезаписати відповідні поля сутності, навіть якщо вони мають значення null.
     @Override
-    public boolean updateAll(RoleDTO roleDTO) {
-        Role role = roleRepository.findById(roleDTO.getId()).orElseThrow();
+    public boolean updateAll(Long id, RoleDTO roleDTO) {
+        Role role = roleRepository.findById(id).orElseThrow();
         roleMapper.fullUpdate(roleDTO, role);
         roleRepository.save(role);
         return true;
@@ -51,15 +52,20 @@ public class RoleService implements CrudService<RoleDTO, Long>{
     //partial update це перевірка на null(Partial Update (Часткове оновлення)
     //Часткове оновлення означає, що оновлюються лише ті поля, які явно задані у DTO. Поля, що мають значення null, не повинні перезаписувати існуючі дані у базі.)
     @Override
-    public boolean update(RoleDTO roleDTO) {
-        Role role = roleRepository.findById(roleDTO.getId()).orElseThrow();
+    public boolean update(Long id, RoleDTO roleDTO) {
+        Role role = roleRepository.findById(id).orElseThrow();
         roleMapper.partialUpdate(roleDTO, role);
         roleRepository.save(role);
         return true;
     }
 
     @Override
-    public void deleteById(Long id) {
-        roleRepository.deleteById(id);
+    public boolean deleteById(Long id) {
+        try {
+            roleRepository.deleteById(id);
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return true;
     }
 }
