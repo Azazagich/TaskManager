b45971c3b43552b5cc7e16db12f655a183217d88
Azazagich <qwertyu2289@ukr.net>
2024-10-11 12:41:08 +0300

    create and add: RoleRepositoryWithBagRelationshipImpl class and add methods findOneRole and findRoleUsers

---
+++ src/main/java/org/example/taskmanager/repository/RoleRepositoryWithBagRelationshipImpl.java
@@ -0,0 +1,39 @@
+package org.example.taskmanager.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.example.taskmanager.domain.Role;
+import org.example.taskmanager.domain.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+import java.util.stream.Collector;
+
+@Repository
+public class RoleRepositoryWithBagRelationshipImpl implements RoleRepositoryWithBagRelationship {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public Optional<Role> findOneRole(Long id) {
+        final String QUERY = "select role from Role role where role.id = :id";
+        Role role = entityManager.createQuery(QUERY, Role.class)
+                .setParameter("id", id)
+                .getSingleResult();
+        if (role.getUsers().isEmpty()) {
+            role.setUsers(findRoleUsers(role));
+        }
+
+        return Optional.of(role);
+    }
+
+    public Set<User> findRoleUsers(Role role){
+        final String QUERY = "select u from User u join fetch u.role where u.role = :role";
+        return new HashSet<>(entityManager
+                .createQuery(QUERY, User.class)
+                .setParameter("role", role)
+                .getResultList());
+    }
+}
