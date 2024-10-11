7e471921e521145080c57201aeabc5f54e85f93b
Azazagich <qwertyu2289@ukr.net>
2024-10-11 12:32:39 +0300

    add: custome method toDTO

--- src/main/java/org/example/taskmanager/service/mapper/RoleMapper.java
+++ src/main/java/org/example/taskmanager/service/mapper/RoleMapper.java
@@ -1,16 +1,42 @@
 package org.example.taskmanager.service.mapper;
 
 import org.example.taskmanager.domain.Role;
+import org.example.taskmanager.domain.User;
 import org.example.taskmanager.service.dto.RoleDTO;
+import org.example.taskmanager.service.dto.UserDTO;
 import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
 
 @Mapper(componentModel = "spring")
 public interface RoleMapper extends MapperEntity<Role, RoleDTO>{
+
+//    @MappingTarget
+//    RoleDTO toDTO(Role role);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void partialUpdate(RoleDTO dto, @MappingTarget Role entity);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
     void fullUpdate(RoleDTO dto, @MappingTarget Role entity);
+
+    @Named("customRoleMapper")
+    @Mapping(target = "users", source = "users", qualifiedByName = "userId")
+    RoleDTO toDTO(Role role);
+
+    @Named("userId")
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "firstName", source = "firstName")
+    @Mapping(target = "lastName", source = "lastName")
+    @Mapping(target = "email", source = "email")
+    @Mapping(target = "password", source = "password")
+    @Mapping(target = "role", source = "role"/*ignore = true*/)
+    UserDTO toDtoUserId(User user);
 }
+
+
