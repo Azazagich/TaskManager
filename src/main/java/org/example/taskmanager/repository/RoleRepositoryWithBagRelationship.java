5eb55b29de1cb08163dbda9a408f5f0ac76ab227
Azazagich <qwertyu2289@ukr.net>
2024-10-11 12:39:50 +0300

    create: RoleRepositoryWithBagRelationship interface

---
+++ src/main/java/org/example/taskmanager/repository/RoleRepositoryWithBagRelationship.java
@@ -0,0 +1,9 @@
+package org.example.taskmanager.repository;
+
+import org.example.taskmanager.domain.Role;
+
+import java.util.Optional;
+
+public interface RoleRepositoryWithBagRelationship {
+    Optional<Role> findOneRole(Long id);
+}
