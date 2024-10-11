0a6349aa1aaf49ab467095b545a0b60930c00a7b
Azazagich <qwertyu2289@ukr.net>
2024-10-11 12:38:27 +0300

    add: custome method findOne in RoleRepository interface

--- src/main/java/org/example/taskmanager/repository/RoleRepository.java
+++ src/main/java/org/example/taskmanager/repository/RoleRepository.java
@@ -2,7 +2,19 @@ package org.example.taskmanager.repository;
 
 import org.example.taskmanager.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> { }
+public interface RoleRepository extends RoleRepositoryWithBagRelationship, JpaRepository<Role, Long> {
+
+    default Optional<Role> findOne(@Param("id") Long id){
+        return findOneRole(id);
+    }
+
+//    @Query("select r from Role r join fetch r.users where r.id = :id")
+//    Optional<Role> findOneJPQL(@Param("id") Long id);
+}
