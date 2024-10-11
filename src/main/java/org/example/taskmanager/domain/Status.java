490b779ae124baa9ee7b8a055a3874660017fd1d
Azazagich <qwertyu2289@ukr.net>
2024-10-11 12:20:19 +0300

    update: methods object in class Status

--- src/main/java/org/example/taskmanager/domain/Status.java
+++ src/main/java/org/example/taskmanager/domain/Status.java
@@ -83,16 +83,17 @@ public class Status {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Status status = (Status) o;
-        return Objects.equals(id, status.id) &&
-                Objects.equals(name, status.name) &&
-                Objects.equals(tasks, status.tasks);
+        if (this == o){
+            return true;
+        }
+        if (!(o instanceof Status)){
+            return false;
+        }
+        return id == ((Status)o).id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, tasks);
+        return getClass().hashCode();
     }
 }
