27f0e7696ef11cfa4fea606a442af5efaa5b68f2
Azazagich <qwertyu2289@ukr.net>
2024-10-15 12:13:46 +0300

    add: customMapper for Status mapper

--- src/main/java/org/example/taskmanager/service/mapper/StatusMapper.java
+++ src/main/java/org/example/taskmanager/service/mapper/StatusMapper.java
@@ -1,11 +1,14 @@
 package org.example.taskmanager.service.mapper;
 
 import org.example.taskmanager.domain.Status;
+import org.example.taskmanager.domain.Tag;
+import org.example.taskmanager.domain.Task;
 import org.example.taskmanager.service.dto.StatusDTO;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.example.taskmanager.service.dto.TagDTO;
+import org.example.taskmanager.service.dto.TaskDTO;
+import org.mapstruct.*;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface StatusMapper extends MapperEntity<Status, StatusDTO>{
@@ -15,4 +18,26 @@ public interface StatusMapper extends MapperEntity<Status, StatusDTO>{
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
     void fullUpdate(StatusDTO dto, @MappingTarget Status entity);
+
+    @Named("taskDTOMap")
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "title", source = "title")
+    @Mapping(target = "body", source = "body")
+    @Mapping(target = "startDate", source = "startDate")
+    @Mapping(target = "finishDate", source = "finishDate")
+    @Mapping(target = "status", source = "status")
+    @Mapping(target = "createBy", source = "createBy")
+    @Mapping(target = "performers", source = "performers")
+    @Mapping(target = "tags", source = "tags")
+    TaskDTO toDtoTask(Task task);
+
+    @Named("customStatusMapper")
+    @Mapping(target = "tasks", source = "tasks", qualifiedByName = "taskDTOMap")
+    StatusDTO toDTO(Status status);
+
+    @Named("customStatusesMapper")
+    @IterableMapping(qualifiedByName = "customStatusMapper")
+    List<StatusDTO> toDTOS(List<Status> statuses);
+
 }
