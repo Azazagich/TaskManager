707bbc0ee1a1d4b4ef3ab2448d88ac742e273a64
Azazagich <qwertyu2289@ukr.net>
2024-10-15 12:09:11 +0300

    comments: useless method in  UserRepository

--- src/main/java/org/example/taskmanager/repository/UserRepository.java
+++ src/main/java/org/example/taskmanager/repository/UserRepository.java
@@ -2,7 +2,15 @@ package org.example.taskmanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.example.taskmanager.domain.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{ }
+public interface UserRepository extends JpaRepository<User, Long> {
+
+//    @Query("select u from User u left join u.tasks where u.id = :id")
+//    Optional<User> findOne(@Param("id") Long id);
+}
