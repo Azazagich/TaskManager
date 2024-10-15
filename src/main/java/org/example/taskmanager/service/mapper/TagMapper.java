e77b319d23c255f3d80da6d04ec76b64fb1f46de
Azazagich <qwertyu2289@ukr.net>
2024-10-15 12:15:26 +0300

    add: customMapper for TagMapper mapper

--- src/main/java/org/example/taskmanager/service/mapper/TagMapper.java
+++ src/main/java/org/example/taskmanager/service/mapper/TagMapper.java
@@ -1,11 +1,14 @@
 package org.example.taskmanager.service.mapper;
 
 import org.example.taskmanager.domain.Tag;
+import org.example.taskmanager.domain.Task;
+import org.example.taskmanager.domain.User;
 import org.example.taskmanager.service.dto.TagDTO;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.example.taskmanager.service.dto.TaskDTO;
+import org.example.taskmanager.service.dto.UserDTO;
+import org.mapstruct.*;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TagMapper extends MapperEntity<Tag, TagDTO>{
@@ -15,4 +18,26 @@ public interface TagMapper extends MapperEntity<Tag, TagDTO>{
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
     void fullUpdate(TagDTO dto, @MappingTarget Tag entity);
+
+    @Named("taskDTOMap")
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "title", source = "title")
+    @Mapping(target = "body", source = "body")
+    @Mapping(target = "startDate", source = "startDate")
+    @Mapping(target = "finishDate", source = "finishDate")
+    @Mapping(target = "createBy", source = "createBy")
+    @Mapping(target = "performers", source = "performers")
+    @Mapping(target = "tags", source = "tags")
+    TaskDTO toDtoTask(Task task);
+
+
+    @Named("customTagMapper")
+    @Mapping(target = "tasks", source = "tasks", qualifiedByName = "taskDTOMap")
+    TagDTO toDTO(Tag tag);
+
+
+    @Named("customTagsMapper")
+    @IterableMapping(qualifiedByName = "customTagMapper")
+    List<TagDTO> toDTOS(List<Tag> tags);
 }
