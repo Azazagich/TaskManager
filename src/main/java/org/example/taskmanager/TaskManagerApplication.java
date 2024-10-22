fc4be30a7573deee844700ea73f6668e0795cc58
Azazagich <qwertyu2289@ukr.net>
2024-10-22 11:12:15 +0300

    set: annotation @EnableJpaREpository for TaskManagerApplication

--- src/main/java/org/example/taskmanager/TaskManagerApplication.java
+++ src/main/java/org/example/taskmanager/TaskManagerApplication.java
@@ -1,20 +1,13 @@
 package org.example.taskmanager;
 
-import org.example.taskmanager.domain.Role;
-import org.example.taskmanager.domain.User;
-import org.example.taskmanager.repository.RoleRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.util.HashSet;
-import java.util.Set;
-
+@EnableJpaRepositories
 @SpringBootApplication
 public class TaskManagerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
-
-
 }
