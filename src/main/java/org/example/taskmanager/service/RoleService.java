94c0977cc66752f4b750e88ee99b1136bf4853aa
Azazagich <qwertyu2289@ukr.net>
2024-10-22 11:09:46 +0300

    update: methods in RoleService

--- src/main/java/org/example/taskmanager/service/RoleService.java
+++ src/main/java/org/example/taskmanager/service/RoleService.java
@@ -1,10 +1,9 @@
 package org.example.taskmanager.service;
 
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.persistence.PersistenceContext;
 import org.example.taskmanager.domain.Role;
 import org.example.taskmanager.repository.RoleRepository;
+import org.example.taskmanager.repository.UserRepository;
 import org.example.taskmanager.service.dto.RoleDTO;
 import org.example.taskmanager.service.mapper.RoleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,17 +18,21 @@ public class RoleService implements CrudService<RoleDTO, Long>{
 
     private final RoleRepository roleRepository;
 
+    private final UserRepository userRepository;
+
     private final RoleMapper roleMapper;
 
+
     @Autowired
-    public RoleService(RoleRepository roleRepository, RoleMapper roleMapper){
+    public RoleService(RoleRepository roleRepository, UserRepository userRepository, RoleMapper roleMapper){
         this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
         this.roleMapper = roleMapper;
     }
 
     @Override
     public RoleDTO getById(Long id) {
-        Role role = roleRepository.findOne(id).orElseThrow();
+        Role role = roleRepository.findById(id).orElseThrow();
         return roleMapper.toDTO(role);
     }
 
@@ -46,26 +49,27 @@ public class RoleService implements CrudService<RoleDTO, Long>{
     //full update це без перевірки на null просто сетається Full Update (Повне оновлення)
     //Повне оновлення означає, що всі поля DTO повинні перезаписати відповідні поля сутності, навіть якщо вони мають значення null.
     @Override
-    public boolean updateAll(Long id, RoleDTO roleDTO) {
-        Role role = roleRepository.findOne(id).orElseThrow();
+    public RoleDTO updateAll(Long id, RoleDTO roleDTO) {
+        Role role = roleRepository.findById(id).orElseThrow();
         roleMapper.fullUpdate(roleDTO, role);
         roleRepository.save(role);
-        return true;
+        return roleMapper.toDTO(roleRepository.findById(id).orElseThrow());
     }
 
     //partial update це перевірка на null(Partial Update (Часткове оновлення)
     //Часткове оновлення означає, що оновлюються лише ті поля, які явно задані у DTO. Поля, що мають значення null, не повинні перезаписувати існуючі дані у базі.)
     @Override
-    public boolean update(Long id, RoleDTO roleDTO) {
-        Role role = roleRepository.findOne(id).orElseThrow();
+    public RoleDTO update(Long id, RoleDTO roleDTO) {
+        Role role = roleRepository.findById(id).orElseThrow();
         roleMapper.partialUpdate(roleDTO, role);
         roleRepository.save(role);
-        return true;
+        return roleMapper.toDTO(roleRepository.findById(id).orElseThrow());
     }
 
     @Override
     public void deleteById(Long id) {
         try {
+            userRepository.updateToNullUsers(id);
             roleRepository.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
             throw new EntityNotFoundException("Role with id " + id + " not found");
