2ed4e340948639423a8f54627dfc219dabcc031d
Azazagich <qwertyu2289@ukr.net>
2024-10-22 10:57:49 +0300

    remove: useless methods such as: (findOne, findOneJPQL)

--- src/main/java/org/example/taskmanager/repository/RoleRepository.java
+++ src/main/java/org/example/taskmanager/repository/RoleRepository.java
@@ -2,19 +2,13 @@ package org.example.taskmanager.repository;
 
 import org.example.taskmanager.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
-public interface RoleRepository extends /*RoleRepositoryWithBagRelationship,*/ JpaRepository<Role, Long> {
-
-//    default Optional<Role> findOne(@Param("id") Long id){
-//        return findOneRole(id);
-//    }
-
-//    @Query("select r from Role r join fetch r.users where r.id = :id")
-//    Optional<Role> findOneJPQL(@Param("id") Long id);
-}
+public interface RoleRepository extends /*RoleRepositoryWithBagRelationship,*/ JpaRepository<Role, Long> { }
