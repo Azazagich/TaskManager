12f4894a061be62723053e6f2c4541b860b5c532
Azazagich <qwertyu2289@ukr.net>
2024-10-22 11:07:36 +0300

    add: methods toLazyDTO, toEntityUser, toEntityTag, toEntityStatus, toEntity, toEntities

--- src/main/java/org/example/taskmanager/service/mapper/TaskMapper.java
+++ src/main/java/org/example/taskmanager/service/mapper/TaskMapper.java
@@ -1,16 +1,15 @@
 package org.example.taskmanager.service.mapper;
 
-import org.example.taskmanager.domain.Status;
-import org.example.taskmanager.domain.Tag;
-import org.example.taskmanager.domain.Task;
-import org.example.taskmanager.domain.User;
-import org.example.taskmanager.service.dto.StatusDTO;
-import org.example.taskmanager.service.dto.TagDTO;
-import org.example.taskmanager.service.dto.TaskDTO;
-import org.example.taskmanager.service.dto.UserDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.example.taskmanager.domain.*;
+import org.example.taskmanager.service.dto.*;
 import org.mapstruct.*;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper extends MapperEntity<Task, TaskDTO>{
@@ -21,17 +20,50 @@ public interface TaskMapper extends MapperEntity<Task, TaskDTO>{
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
     void fullUpdate(TaskDTO dto, @MappingTarget Task entity);
 
+    //////////////////////////////////////////////////
 
     @Named("customTaskMapper")
-    @Mapping(target = "performers", source = "performers", qualifiedByName = "userDTOMap")
-    @Mapping(target = "createBy", source = "createBy", qualifiedByName = "userDTOMap")
-    @Mapping(target = "tags", source = "tags", qualifiedByName = "tagDTOMap")
-    @Mapping(target = "status", source = "status", qualifiedByName = "statusDTOMap")
+    @Mapping(target = "performers", source = "performers", qualifiedByName = "userMap")
+    @Mapping(target = "createBy", source = "createBy", qualifiedByName = "userMap")
+    @Mapping(target = "tags", source = "tags", qualifiedByName = "tagMap")
+    @Mapping(target = "status", source = "status", qualifiedByName = "statusMap")
     TaskDTO toDTO(Task task);
 
+    @Named("LazyTaskMapper")
+    @Mapping(target = "performers", ignore = true)
+    @Mapping(target = "createBy", ignore = true)
+    @Mapping(target = "tags", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    TaskDTO toLazyDTO(Task task);
+
     @Named("customTasksMapper")
-    @IterableMapping(qualifiedByName = "customTaskMapper")
+    @IterableMapping(qualifiedByName = "LazyTaskMapper")
     List<TaskDTO> toDTOS(List<Task> tasks);
+
+    @Named("userMap")
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "firstName", source = "firstName")
+    @Mapping(target = "lastName", source = "lastName")
+    @Mapping(target = "email", source = "email")
+    @Mapping(target = "password", source = "password")
+    @Mapping(target = "role", ignore = true)
+    UserDTO toDtoUser(User user);
+
+    @Named("tagMap")
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "name", source = "name")
+    TagDTO toDtoTag(Tag tag);
+
+    @Named("statusMap")
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "name", source = "name")
+    StatusDTO toDtoStatus(Status status);
+
+/////////////////////////
+
 
     @Named("userDTOMap")
     @BeanMapping(ignoreByDefault = true)
@@ -40,18 +72,31 @@ public interface TaskMapper extends MapperEntity<Task, TaskDTO>{
     @Mapping(target = "lastName", source = "lastName")
     @Mapping(target = "email", source = "email")
     @Mapping(target = "password", source = "password")
-    UserDTO toDtoUser(User user);
+    @Mapping(target = "role", ignore = true)
+    User toEntityUser(UserDTO userDTO);
 
     @Named("tagDTOMap")
     @BeanMapping(ignoreByDefault = true)
     @Mapping(target = "id", source = "id")
     @Mapping(target = "name", source = "name")
-    TagDTO toDtoTag(Tag tag);
+    Tag toEntityTag(TagDTO tagDTO);
 
     @Named("statusDTOMap")
     @BeanMapping(ignoreByDefault = true)
     @Mapping(target = "id", source = "id")
     @Mapping(target = "name", source = "name")
-    StatusDTO toDtoStatus(Status status);
+    Status toEntityStatus(StatusDTO statusDTO);
 
+
+    @Named("customTaskDTOMapper")
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "performers", source = "performers", qualifiedByName = "userDTOMap")
+    @Mapping(target = "createBy", source = "createBy", qualifiedByName = "userDTOMap")
+    @Mapping(target = "tags", source = "tags", qualifiedByName = "tagDTOMap")
+    @Mapping(target = "status", source = "status", qualifiedByName = "statusDTOMap")
+    Task toEntity(TaskDTO taskDTO);
+
+    @Named("customTasksDTOMapper")
+    @IterableMapping(qualifiedByName = "customTaskDTOMapper")
+    List<Task> toEntities(List<TaskDTO> tasks);
 }
