38fe3d8b006992c8c2f1e263af1873c702a752fa
Azazagich <qwertyu2289@ukr.net>
2024-10-22 11:12:54 +0300

    update: methods in UserService

--- src/main/java/org/example/taskmanager/service/UserService.java
+++ src/main/java/org/example/taskmanager/service/UserService.java
@@ -1,20 +1,19 @@
 package org.example.taskmanager.service;
 
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.persistence.PreRemove;
-import org.example.taskmanager.domain.Task;
 import org.example.taskmanager.domain.User;
 import org.example.taskmanager.repository.TaskRepository;
 import org.example.taskmanager.repository.UserRepository;
 import org.example.taskmanager.service.dto.UserDTO;
 import org.example.taskmanager.service.mapper.UserMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -22,14 +21,19 @@ public class UserService implements CrudService<UserDTO, Long>{
 
     private final UserRepository userRepository;
 
-   // private final TaskRepository taskRepository;
-
     private final UserMapper userMapper;
 
+    private final RoleService roleService;
+
+    private final TaskRepository taskRepository;
+
+    final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     @Autowired
-    public UserService(UserRepository userRepository, TaskRepository taskRepository, UserMapper userMapper){
+    public UserService(UserRepository userRepository, RoleService roleService, TaskRepository taskRepository, UserMapper userMapper){
         this.userRepository = userRepository;
-  //      this.taskRepository = taskRepository;
+        this.roleService = roleService;
+        this.taskRepository = taskRepository;
         this.userMapper = userMapper;
     }
 
@@ -45,30 +49,33 @@ public class UserService implements CrudService<UserDTO, Long>{
 
     @Override
     public UserDTO save(UserDTO userDTO) {
-        return userMapper.toDTO(userRepository.save(userMapper.toEntity(userDTO)));
+        UserDTO dto = userMapper.toDTO(userRepository.save(userMapper.toEntity(userDTO)));
+        roleService.update(userDTO.getRole().getId(), userDTO.getRole());
+        return dto;
     }
 
     @Override
-    public boolean updateAll(Long id, UserDTO userDTO) {
+    public UserDTO updateAll(Long id, UserDTO userDTO) {
         User user = userRepository.findById(id).orElseThrow();
         userMapper.fullUpdate(userDTO, user);
         userRepository.save(user);
-        return true;
+        return userMapper.toDTO(userRepository.findById(id).orElseThrow());
     }
 
     @Override
-    public boolean update(Long id, UserDTO userDTO) {
+    public UserDTO update(Long id, UserDTO userDTO) {
         User user = userRepository.findById(id).orElseThrow();
         userMapper.partialUpdate(userDTO, user);
         userRepository.save(user);
-        return true;
+        return userMapper.toDTO(userRepository.findById(id).orElseThrow());
     }
 
     @Override
     public void deleteById(Long id) {
         try {
-            //taskRepository.updateToNullTasksByCreateById(id);
-            //taskRepository.updateToNullTasksPerformers(id);
+//            User user = userRepository.findById(id).orElseThrow();
+//            user.getTasks().forEach(task -> task.setPerformers(null));
+            taskRepository.updateToNullCreateBy(id);
             userRepository.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
             throw new EntityNotFoundException("Role with id " + id + " not found");
