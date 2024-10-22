2bc86ac5764ebc11107adbb6a16d77b384bf22bd
Azazagich <qwertyu2289@ukr.net>
2024-10-22 11:12:36 +0300

    update: methods in TaskService

--- src/main/java/org/example/taskmanager/service/TaskService.java
+++ src/main/java/org/example/taskmanager/service/TaskService.java
@@ -1,11 +1,13 @@
 package org.example.taskmanager.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.example.taskmanager.domain.Task;
-import org.example.taskmanager.domain.User;
 import org.example.taskmanager.repository.TaskRepository;
+import org.example.taskmanager.repository.UserRepository;
 import org.example.taskmanager.service.dto.TaskDTO;
 import org.example.taskmanager.service.mapper.TaskMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,13 +18,19 @@ import java.util.List;
 @Transactional
 public class TaskService implements CrudService<TaskDTO, Long>{
 
+    private final UserService userService;
+
     private final TaskRepository taskRepository;
+
+    private final UserRepository userRepository;
 
     private final TaskMapper taskMapper;
 
     @Autowired
-    public TaskService(TaskRepository taskRepository, TaskMapper taskMapper){
+    public TaskService(UserService userService, TaskRepository taskRepository, UserRepository userRepository, TaskMapper taskMapper){
+        this.userService = userService;
         this.taskRepository = taskRepository;
+        this.userRepository = userRepository;
         this.taskMapper = taskMapper;
     }
 
@@ -33,34 +41,43 @@ public class TaskService implements CrudService<TaskDTO, Long>{
 
     @Override
     public List<TaskDTO> getAll() {
-        return taskMapper.toDTOS(taskRepository.findAll());
+        List<Task> all = taskRepository.findAll();
+        return taskMapper.toDTOS(all);
     }
 
+    //#
     @Override
     public TaskDTO save(TaskDTO taskDTO) {
-            return taskMapper.toDTO(taskRepository.save(taskMapper.toEntity(taskDTO)));
+        Task dto = taskMapper.toEntity(taskDTO);
+        Task save = taskRepository.save(dto);
+        return taskMapper.toDTO(save);
     }
 
     @Override
-    public boolean updateAll(Long id, TaskDTO taskDTO) {
+    public TaskDTO updateAll(Long id, TaskDTO taskDTO) {
         Task task = taskRepository.findById(id).orElseThrow();
         taskMapper.fullUpdate(taskDTO, task);
         taskRepository.save(task);
-        return true;
+        return taskMapper.toDTO(taskRepository.findById(id).orElseThrow());
     }
 
     @Override
-    public boolean update(Long id, TaskDTO taskDTO) {
+    public TaskDTO update(Long id, TaskDTO taskDTO) {
         Task task = taskRepository.findById(id).orElseThrow();
         taskMapper.partialUpdate(taskDTO, task);
         taskRepository.save(task);
-        return true;
+        return taskMapper.toDTO(taskRepository.findById(id).orElseThrow());
     }
 
     @Override
     public void deleteById(Long id) {
         Task task = taskRepository.findById(id).orElseThrow();
-        task.getPerformers();
-        taskRepository.deleteById(id);
+        //task.getPerformers().forEach(performers -> performers.setTasks(null));
+        try {
+            taskRepository.deleteById(id);
+        } catch (
+            EmptyResultDataAccessException e) {
+            throw new EntityNotFoundException("Role with id " + id + " not found");
+        }
     }
 }
