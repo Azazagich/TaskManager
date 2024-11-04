0997e0c9ba7a22d7329e833ab8f3f391fe1662e5
Azazagich <qwertyu2289@ukr.net>
2024-11-04 17:43:07 +0200

    create: AuthenticationService class

---
+++ src/main/java/org/example/taskmanager/service/AuthenticationService.java
@@ -0,0 +1,52 @@
+package org.example.taskmanager.service;
+
+import org.example.taskmanager.domain.User;
+import org.example.taskmanager.repository.UserRepository;
+import org.example.taskmanager.service.dto.LoginUserDto;
+import org.example.taskmanager.service.dto.RegisterUserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthenticationService(
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder
+            ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(RegisterUserDTO input) {
+        User user = new User()
+                .firstName(input.getFirstName())
+                .email(input.getEmail())
+                .password(passwordEncoder.encode(input.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserDto input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        return userRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+    }
+}
