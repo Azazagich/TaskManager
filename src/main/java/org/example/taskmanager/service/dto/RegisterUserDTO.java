2d9aa0f8dd00daee3b65342ec5a3250ee608249b
Azazagich <qwertyu2289@ukr.net>
2024-11-04 17:45:16 +0200

    create: RegisterUserDTO classDTO

--- src/main/java/org/example/taskmanager/service/dto/RegisterUserDTO.java
+++ src/main/java/org/example/taskmanager/service/dto/RegisterUserDTO.java
@@ -1,62 +1,51 @@
-//package org.example.taskmanager.service.dto;
-//
-//import java.io.Serializable;
-//import java.util.Objects;
-//
-//public class RegisterUserDTO implements Serializable {
-//    private String email;
-//
-//    private String password;
-//
-//    private String fullName;
-//
-//    public RegisterUserDTO(){ }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public String getFullName() {
-//        return fullName;
-//    }
-//
-//    public void setFullName(String fullName) {
-//        this.fullName = fullName;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "RegisterUserDTO{" +
-//                "email='" + email + '\'' +
-//                ", password='" + password + '\'' +
-//                ", fullName='" + fullName + '\'' +
-//                '}';
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        RegisterUserDTO that = (RegisterUserDTO) o;
-//        return Objects.equals(email, that.email) &&
-//                Objects.equals(password, that.password) &&
-//                Objects.equals(fullName, that.fullName);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(email, password, fullName);
-//    }
-//}
+package org.example.taskmanager.service.dto;
+
+public class RegisterUserDTO {
+
+    private String email;
+
+    private String password;
+
+    private String firstName;
+
+    public RegisterUserDTO() { }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public RegisterUserDTO email(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public RegisterUserDTO password(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public RegisterUserDTO firstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
