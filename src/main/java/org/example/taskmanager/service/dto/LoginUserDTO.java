ace53ec4db35ed287104a718cfabb47c77b1b865
Azazagich <qwertyu2289@ukr.net>
2024-11-04 17:45:06 +0200

    create: LoginUserDto classDTO

--- src/main/java/org/example/taskmanager/service/dto/LoginUserDTO.java
+++ src/main/java/org/example/taskmanager/service/dto/LoginUserDTO.java
@@ -1,36 +1,27 @@
-//package org.example.taskmanager.service.dto;
-//
-//import java.io.Serializable;
-//import java.util.Objects;
-//
-//public class LoginUserDTO implements Serializable {
-//    private String email;
-//
-//    private String password;
-//
-//    public LoginUserDTO(){ }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "LoginUserDTO{" +
-//                "email='" + email + '\'' +
-//                ", password='" + password + '\'' +
-//                '}';
-//    }
-//}
+package org.example.taskmanager.service.dto;
+
+public class LoginUserDto {
+
+    private String email;
+
+    private String password;
+
+    public LoginUserDto() { }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
