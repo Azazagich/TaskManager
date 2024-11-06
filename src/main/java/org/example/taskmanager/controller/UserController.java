a0c81134f3b723f5cbf0bf1d99da12045d13751e
Azazagich <qwertyu2289@ukr.net>
2024-11-06 11:28:29 +0200

    update: set PreAuthorize in  class UserController

--- src/main/java/org/example/taskmanager/controller/UserController.java
+++ src/main/java/org/example/taskmanager/controller/UserController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.taskmanager.service.UserService;
 import org.example.taskmanager.service.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,36 +21,42 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     @GetMapping("/{id}")
     public UserDTO getUser(@PathVariable Long id){
         return userService.getById(id);
     }
 
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public List<UserDTO> getAllUsers(){
         return userService.getAll();
     }
 
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public UserDTO saveUser(@RequestBody UserDTO userDTO){
         return userService.save(userDTO);
     }
 
 
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @PutMapping("/{id}")
     public UserDTO fullUpdateUser(@PathVariable Long id, @RequestBody UserDTO userDTO){
         return userService.updateAll(id, userDTO);
     }
 
 
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @PatchMapping("/{id}")
     public UserDTO partialUpdateUser(@PathVariable Long id, @RequestBody UserDTO userDTO){
         return userService.update(id, userDTO);
     }
 
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable Long id){
         userService.deleteById(id);
