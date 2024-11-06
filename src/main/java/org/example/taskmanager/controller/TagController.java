fee407264ade6742f48bead731d8f028369e045f
Azazagich <qwertyu2289@ukr.net>
2024-11-06 11:29:29 +0200

    update: set PreAuthorize in  class TagController and change names in methods

--- src/main/java/org/example/taskmanager/controller/TagController.java
+++ src/main/java/org/example/taskmanager/controller/TagController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.taskmanager.service.TagService;
 import org.example.taskmanager.service.dto.TagDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,39 +21,44 @@ public class TagController {
         this.tagService = tagService;
     }
 
-
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('USER')")
     @GetMapping("/{id}")
-    public TagDTO getRole(@PathVariable Long id){
+    public TagDTO getTag(@PathVariable Long id){
         return tagService.getById(id);
     }
 
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('USER')")
     @GetMapping
-    public List<TagDTO> getAllRoles(){
+    public List<TagDTO> getAllTags(){
         return tagService.getAll();
     }
 
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('USER')")
     @PostMapping
-    public TagDTO saveRole(@RequestBody TagDTO tagDTO){
+    public TagDTO saveTag(@RequestBody TagDTO tagDTO){
         return tagService.save(tagDTO);
     }
 
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('USER')")
     @PutMapping("/{id}")
-    public TagDTO fullUpdateRole(@PathVariable Long id, @RequestBody TagDTO tagDTO){
+    public TagDTO fullUpdateTag(@PathVariable Long id, @RequestBody TagDTO tagDTO){
         return tagService.updateAll(id, tagDTO);
     }
 
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('USER')")
     @PatchMapping("/{id}")
-    public TagDTO partialUpdateRole(@PathVariable Long id, @RequestBody TagDTO tagDTO){
+    public TagDTO partialUpdateTag(@PathVariable Long id, @RequestBody TagDTO tagDTO){
         return tagService.update(id, tagDTO);
     }
 
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('USER')")
     @DeleteMapping("/{id}")
-    public void deleteRole(@PathVariable Long id){
+    public void deleteTag(@PathVariable Long id){
         tagService.deleteById(id);
     }
 }
