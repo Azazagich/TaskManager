18f109b0b7ec01df0e4f6461fa6e25131ee3b403
Azazagich <qwertyu2289@ukr.net>
2024-11-06 11:29:51 +0200

    update: set PreAuthorize in  class StatusController and change names in methods

--- src/main/java/org/example/taskmanager/controller/StatusController.java
+++ src/main/java/org/example/taskmanager/controller/StatusController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.taskmanager.service.StatusService;
 import org.example.taskmanager.service.dto.StatusDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,39 +21,44 @@ public class StatusController {
         this.statusService = statusService;
     }
 
-
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('USER')")
     @GetMapping("/{id}")
-    public StatusDTO getRole(@PathVariable Long id){
+    public StatusDTO getStatus(@PathVariable Long id){
         return statusService.getById(id);
     }
 
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('USER')")
     @GetMapping
-    public List<StatusDTO> getAllRoles(){
+    public List<StatusDTO> getAllStatuses(){
         return statusService.getAll();
     }
 
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('USER')")
     @PostMapping
-    public StatusDTO saveRole(@RequestBody StatusDTO statusDTO){
+    public StatusDTO saveStatus(@RequestBody StatusDTO statusDTO){
         return statusService.save(statusDTO);
     }
 
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('USER')")
     @PutMapping("/{id}")
-    public StatusDTO fullUpdateRole(@PathVariable Long id, @RequestBody StatusDTO statusDTO){
+    public StatusDTO fullUpdateStatus(@PathVariable Long id, @RequestBody StatusDTO statusDTO){
         return statusService.updateAll(id, statusDTO);
     }
 
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('USER')")
     @PatchMapping("/{id}")
-    public StatusDTO partialUpdateRole(@PathVariable Long id, @RequestBody StatusDTO statusDTO){
+    public StatusDTO partialUpdateStatus(@PathVariable Long id, @RequestBody StatusDTO statusDTO){
         return statusService.update(id, statusDTO);
     }
 
 
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('USER')")
     @DeleteMapping("/{id}")
-    public void deleteRole(@PathVariable Long id){
+    public void deleteStatus(@PathVariable Long id){
         statusService.deleteById(id);
     }
 }
